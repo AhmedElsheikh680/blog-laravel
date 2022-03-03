cc603a9c925a43eb71065114a217b0b3a7dba563
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2022-03-03 14:12:36 -0800

    adding middleware in web.php and controller(__construct())

--- app/Http/Controllers/Admin/SecondController.php
+++ app/Http/Controllers/Admin/SecondController.php
@@ -6,7 +6,24 @@ use App\Http\Controllers\Controller;
 
 class SecondController extends Controller
 {
-    public function showString() {
-        return "Static Data";
+
+    public function __construct() {
+        $this->middleware('auth')->except('showString3');
+    }
+
+    public function showString0() {
+        return "Static Data 0";
+    }
+    public function showString1() {
+        return "Static Data 1";
+    }
+    public function showString2() {
+        return "Static Data 2";
+    }
+    public function showString3() {
+        return "Static Data 3";
+    }
+    public function showString4() {
+        return "Static Data 4";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,28 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Front\UserController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Admin\SecondController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Admin'], function(){
+    Route::get('second0', [SecondController::class, 'showString0']);
+    Route::get('second1', [SecondController::class, 'showString1']);
+    Route::get('second2', [SecondController::class, 'showString2']);
+    Route::get('second3', [SecondController::class, 'showString3']);
 });
+Route::get('login', function (){
+   return "You Are Login";
+})->name('login');
 
-Route::get('second', [SecondController::class, 'showString']);
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//Route::group(['namespace' => 'admin'], function() {
+//   Route::get('second', [SecondController::class, 'showString']);
+//});
+
+//Route::get('second', [SecondController::class, 'showString']);
+
 
 //Route::namespace('Front')->group(function() {
 //    //ALl Routes
